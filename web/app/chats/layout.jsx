2a86678d29481fd0cd7fb4a519c7f62427b8d761
Timo Vink <timovink@gmail.com>
2024-08-04 10:11:02 -0400

    Fix bug in timestamp display.

--- web/app/chats/layout.jsx
+++ web/app/chats/layout.jsx
@@ -55,7 +55,7 @@ const ThreadListEntry = ({ thread, isActive }) => (
           {thread.title}
         </div>
         <div className="flex-none text-xs text-muted-foreground">
-          <time dateTime={thread.timestamp} title={format(thread.timestamp, 'EEEE, MMMM q, yyyy @ h:mm a')}>
+          <time dateTime={thread.timestamp} title={format(thread.timestamp, 'EEEE, MMMM d, yyyy @ h:mm a')}>
             {formatDistanceToNow(thread.timestamp, { addSuffix: true })}
           </time>
         </div>

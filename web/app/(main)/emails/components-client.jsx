b792d3639293a9af9179c5dafd0ea1b7398dc083
Timo Vink <timovink@gmail.com>
2024-08-09 10:48:32 -0400

    Improve display of emails.

--- web/app/(main)/emails/components-client.jsx
+++ web/app/(main)/emails/components-client.jsx
@@ -3,9 +3,12 @@
 import { useSearchParams } from 'next/navigation';
 import { Letter } from 'react-letter';
 import { extract } from 'letterparser';
+import { format, formatDistanceToNow } from 'date-fns';
 
 import { ThreadEntriesSkeleton, Card, ThreadEntryScrollContainer } from './components-server';
 import { useApiCall, useTextApiCall } from '@/lib/api';
+
+import './email.css';
 
 
 export const ThreadEntry = ({ entry }) => {
@@ -13,8 +16,24 @@ export const ThreadEntry = ({ entry }) => {
   const { html, text } = extract(data);
 
   return (
-    <Card className="p-4">
-      <Letter html={html} text={text} />
+    <Card id={`entry-${entry.entryId}`}>
+      <div className="p-4 border-b">
+        <div className="flex items-center space-x-1">
+          <div className="flex flex-1 justify-between items-baseline space-x-1">
+            <div className="line-clamp-1 font-semibold">
+              {entry.authorFullName}
+            </div>
+            <div className="flex-none text-xs text-muted-foreground">
+              <time dateTime={entry.timestamp} title={format(entry.timestamp, 'EEEE, MMMM d, yyyy @ h:mm a')}>
+                {formatDistanceToNow(entry.timestamp, { addSuffix: true })}
+              </time>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="p-4 email">
+        <Letter html={html} text={text} />
+      </div>
     </Card>
   );
 };

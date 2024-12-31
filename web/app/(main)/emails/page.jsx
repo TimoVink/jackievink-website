3f740d5341c46365db60df37ab4efe2de0fddef2
Timo Vink <timovink@gmail.com>
2024-12-31 10:05:29 -0500

    Codemod for Next v15.

--- web/app/(main)/emails/page.jsx
+++ web/app/(main)/emails/page.jsx
@@ -1,13 +1,17 @@
 import { Layout, ThreadEntriesSuspenseContainer, ThreadListSuspenseContainer } from './components-server';
 
 
-const Page = ({ searchParams }) => (
-  <Layout
-    threadId={searchParams.id}
-    listComponent={<ThreadListSuspenseContainer threadId={searchParams.id} />}
-    detailComponent={<ThreadEntriesSuspenseContainer threadId={searchParams.id} />}
-  />
-);
+const Page = async props => {
+  const searchParams = await props.searchParams;
+
+  return (
+    <Layout
+      threadId={searchParams.id}
+      listComponent={<ThreadListSuspenseContainer threadId={searchParams.id} />}
+      detailComponent={<ThreadEntriesSuspenseContainer threadId={searchParams.id} />}
+    />
+  );
+};
 
 
 export default Page;
